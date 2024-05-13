4d1398d67924ef2eba8f85c71abf23a4ef49c733
Chiyen Venegas <chi@marandino.dev>
2024-05-13 07:31:31 -0600

    adds: better messaging for deprecated endpoint

--- src/pages/api/cdk-deployer.ts
+++ src/pages/api/cdk-deployer.ts
@@ -20,13 +20,16 @@ export default async function handler(
 
     const highestCount = Math.max(...developers.map((developer) => developer.count));
 
-  const filteredDevelopers = developers.filter((developer) => developer.count < highestCount)
+    const currentDeveloper = developers.find((developer) => developer.current);
+
+    const filteredDevelopers = developers.filter((developer) => developer.count < highestCount)
     if(filteredDevelopers.length === 0) filteredDevelopers.push(...developers);
 
     const selectedDeveloper = filteredDevelopers[Math.floor(Math.random() * filteredDevelopers.length)];
     console.log("selected: ", selectedDeveloper?.name)
 
     // up the count on the selected developer on the developers file and write it down with qsl
+
     await sql`UPDATE developers SET count = count + 1 WHERE name = ${selectedDeveloper.name}`;
     await sql`UPDATE developers set current = true WHERE name = ${selectedDeveloper.name}`;
     await sql`UPDATE developers set current = false WHERE name != ${selectedDeveloper.name}`;
@@ -34,9 +37,12 @@ export default async function handler(
   return res.status(200).json(
     {
       response_type: 'in_channel',
-      text: `The @cdk-deployer for this week will be: <@${selectedDeveloper.slack_id}>`,
+      text: `This Week's CDK Deployer:  <@${selectedDeveloper.slack_id}> | Backup: <@${currentDeveloper?.slack_id}>.`,
     }
   );
+
+    // TODO: add a check that returns who is the current developer
+    // TODO: add some sort of log where it'd be ordered by date, to keep track of dates as the "current" might not be enough
     
   } catch (error) {
     return res.status(500).json({message: 'Internal server error', error: JSON.stringify(error)});
