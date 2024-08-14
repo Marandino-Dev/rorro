16f57f67dbedbf6f43014077a2f705928298fccf
K3iiX <karaya@marandino.dev>
2024-08-14 09:55:16 -0600

    adds: head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import { Navbar } from "./api/components/navbar";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Head>
+        <title>Rorro</title>
+        <meta name="description" content="Round robin tool" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
 }
-
