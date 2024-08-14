b5bf1c0acf750164e67d127dac91942785b372a5
Chiyen Venegas <chi@marandino.dev>
2024-08-13 21:49:24 -0600

    adds: database helper and initial work for the rotation post

--- src/pages/api/v1/rotation/[rotationName]/index.ts
+++ src/pages/api/v1/rotation/[rotationName]/index.ts
@@ -1,5 +1,16 @@
+import { PostgresClient, TableName } from "@/pages/api/_utils/database";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { logRequest, returnInvalidRequest } from "utils/server-helper";
+
+type SlackUser = {
+  slackId: string;
+  fullName: string;
+  count: number;
+  holiday: boolean;
+  current: boolean;
+  backup: boolean
+}
+
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,16 +18,28 @@ export default async function handler(
 ) {
 
   logRequest(req)
-
+  const { rotationName } = req.query
+  if (!rotationName || typeof rotationName !== 'string') return res.status(400).json({ error: 'rotationName is required' });
 
   switch (req.method) {
     case "POST":
-      // TODO: add the handler so this can rotate the user.
-      returnInvalidRequest(res)
-      break;
+      // TODO: get the organization name from the slack request instead.
+      // TODO: fetch the users in the channel this was created on.
+      const DbClient = new PostgresClient('marandino', rotationName)// TODO: softcode organization
+      //TODO: softcode the user
+      const users = await DbClient.putItem<SlackUser>(
+        {
+          slackId: 'U01J3T6QZ9J',
+          fullName: 'Marcelo',
+          count: 0,
+          holiday: false,
+          current: false,
+          backup: false
+        }, TableName.Users);
+      console.log(users)
+      return res.status(200).json(users)
 
     case "GET":
-      //TODO: return the current active and backup users.
       returnInvalidRequest(res)
       break;
 
