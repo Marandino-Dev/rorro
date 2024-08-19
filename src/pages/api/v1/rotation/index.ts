3f17c55b62bc8a5048fec57174e39b78aa22e37f
Deyner Cruz <nenedac98@gmail.com>
2024-08-19 13:34:11 -0600

    adds: initial work on rotation get

--- src/pages/api/v1/rotation/index.ts
+++ src/pages/api/v1/rotation/index.ts
@@ -1,28 +1,56 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { logRequest, returnInvalidRequest } from "utils/server-helper";
+import { getSlackMessage, SlackResponseType } from "utils/slack";
+import { sql } from "@vercel/postgres";
 
+async function getCurrentRotations(res: NextApiResponse) {
+  try {
+    const { rows: logs } = await sql`SELECT * FROM marandino_standup_logs ORDER BY date DESC LIMIT 1`;
+
+    if (!logs || logs.length === 0) {
+      const errorMessage = getSlackMessage(SlackResponseType.Ephemeral, 'No logs found');
+      return res.status(404).json(errorMessage);
+    }
+
+    const lastLog = logs[0];
+
+    const slackMessage = getSlackMessage(
+      SlackResponseType.Ephemeral,
+      `Slack user on duty: <@${lastLog.current_deployer}> | Backup Slack user: <@${lastLog.backup_deployer}>.`
+    );
+
+    return res.status(200).json(slackMessage);
+  }
+  catch (error) {
+    console.error(JSON.stringify(error));
+    const slackMessage = getSlackMessage(
+      SlackResponseType.Ephemeral, 'Something went wrong, please try again.'
+    );
+    return res.status(500).json(slackMessage);
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<unknown>,
 ) {
 
-  logRequest(req)
+  logRequest(req);
 
 
   switch (req.method) {
     case "POST":
       // TODO: add the handler for creating a rotation instead of this.
-      returnInvalidRequest(res)
+      returnInvalidRequest(res);
       break;
 
     case "GET":
       //TODO: add a handler for returning the current rotations for this user.
-      returnInvalidRequest(res)
+      await getCurrentRotations(res);
       break;
 
     default:
-      returnInvalidRequest(res)
+      returnInvalidRequest(res);
       break;
   }
 }
