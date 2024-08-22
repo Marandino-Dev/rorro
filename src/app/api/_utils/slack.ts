551e53113c4130e5b7557f215f29363b09defcf3
Chiyen Venegas <chi@marandino.dev>
2024-08-22 08:26:59 -0600

    RR-15 - Slack Rotation Post (#14)
    * renames: rotation post to a new path
    
    * adds: slack-rotation-post
    
    * adds: eslintrc file, and fixes some of the code

--- src/app/api/_utils/slack.ts
+++ src/app/api/_utils/slack.ts
@@ -1,3 +1,6 @@
+import type { NextRequest } from "next/server";
+import { SlackUser, SlackCommandPayload } from "types"
+
 /** Slack Response Type in order to choose what type of display the return message will have */
 export enum SlackResponseType {
   /** Used to return a message in the same channel the command was executed. Everyone can see it */
@@ -16,5 +19,70 @@ export function getSlackMessage(responseType: SlackResponseType, responseText: s
     text: responseText,
   }
 
+}
+
+
+export async function getSlackUsersFromChannel(channel: string): Promise<SlackUser[]> {
+  //testing <@U07F36MJVFH|dcruz> <!here> <!subteam^S07JMHLTDHN|@backend>
+  // const regex = /<!subteam\^([^|]+)/;
+  // const userGroupId = text.match(regex)?.[1]
+  //
+
+  //TODO: make it accept an user group
+  if (!channel) return []
+
+  const token = process.env.SLACK_TEST_TOKEN;
+  // const res = await fetch('https://slack.com/api/usergroups.users.list?usergroup=' + userGroupId,
+  const res = await fetch('https://slack.com/api/conversations.members?channel=' + channel,
+    {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + token,//TODO: we need to take the token we stored upon installation, not this one.
+      },
+    }
+  )
+
+  const responseData = await res.json()// this should be able to return just that. slackId[]
+  const { members: slackUserIds } = responseData;
+
+  console.debug(slackUserIds)
+  if (slackUserIds?.length < 0) return []
+  return slackUserIds.map((slackId: string) => createUser(slackId));
+}
+
+function createUser(slackId: string): SlackUser {
+  const user: SlackUser = {
+    slackId,
+    fullName: '', //TODO: add the fullname.
+    count: 0,
+    onDuty: false,
+    backup: false,
+    holiday: false,
+  }
+  console.debug('The user has been created for: ', slackId)
+  return user
+}
+
+
+/** Returns the input from the user, without spaces so the table doesn't break */
+export function sanitizeSlackText(text: string) {
+  console.debug("I'm sanitizing the following text:", text)
+  return text.trim().toLowerCase().replaceAll(" ", "_").replaceAll('-', '_')
+}
+
+export async function parsePayloadFromRequest(req: NextRequest): Promise<SlackCommandPayload> {
+  const contentType = req.headers.get('content-type');
+
+  if (contentType?.includes('application/json')) {
+    // Handle JSON data
+    return await req.json() as SlackCommandPayload
+  }
+  if (contentType?.includes('multipart/form-data') || contentType?.includes('application/x-www-form-urlencoded')) {
+    // Handle form data
+    const formData = await req.formData();
+    return Object.fromEntries(formData) as SlackCommandPayload
+  }
+
+  throw new Error('We failed to parse the command with contentType: ' + contentType)
 }
 
