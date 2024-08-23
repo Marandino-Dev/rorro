c5493cf9986b46e14fc9f68bf5c9a6a74f4af03b
Chiyen Venegas <chi@marandino.dev>
2024-08-23 15:00:52 -0600

    adds: linting rules and fixes unused vars

--- src/app/api/cdk.ts
+++ src/app/api/cdk.ts
@@ -2,10 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { sql } from "@vercel/postgres";
 
-type Developer = {
-  name: string;
-  count: number;
-};
 
 export const runtime = 'nodejs';
 
@@ -13,12 +9,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<unknown>,
 ) {
-  const {text = ''} = req.body;
+  const { text = '' } = req.body;
   // const arguments = text.split(' ').slice(1);
   const command = text.split(' ')[0];
 
   try {
-    
+
     switch (command) {
       case 'list':
         return listHandler(req, res);
@@ -40,27 +36,27 @@ export default async function handler(
     return res.status(500).json({ message: 'Internal server error', error: JSON.stringify(error) });
   }
 }
-const listHandler = async function(
+const listHandler = async function (
   req: NextApiRequest,
   res: NextApiResponse<unknown>,
 ) {
   try {
-    const {rows : developers} = await sql`SELECT * FROM developers`;
-   if(!developers) return res.status(404).json({message: 'No developers found'});
+    const { rows: developers } = await sql`SELECT * FROM developers`;
+    if (!developers) return res.status(404).json({ message: 'No developers found' });
 
-  return res.status(200).json(
-    {
-      response_type: 'ephemeral',
-      // it should return the list of developers with their count
-      text: developers.map((developer) => `${developer.name}: ${developer.count}`).join('\n')
-    }
-  );
+    return res.status(200).json(
+      {
+        response_type: 'ephemeral',
+        // it should return the list of developers with their count
+        text: developers.map((developer) => `${developer.name}: ${developer.count}`).join('\n')
+      }
+    );
 
     // TODO: add a check that returns who is the current developer
     // TODO: add some sort of log where it'd be ordered by date, to keep track of dates as the "current" might not be enough
-    
+
   } catch (error) {
-    return res.status(500).json({message: 'Internal server error', error: JSON.stringify(error)});
+    return res.status(500).json({ message: 'Internal server error', error: JSON.stringify(error) });
   }
 }
 
@@ -71,25 +67,25 @@ export async function assignHandler(
 ) {
   const authorizedUsers = ["U0467DXP7D5", "U065JMM3WEB"]
   if (!authorizedUsers.includes(req.body.user_id))
-    return res.status(200).json({text: 'You are not authorized to run this command.'});
+    return res.status(200).json({ text: 'You are not authorized to run this command.' });
   try {
-    const {rows : developers} = await sql`SELECT * FROM developers`;
+    const { rows: developers } = await sql`SELECT * FROM developers`;
 
-   if(!developers) return res.status(404).json({message: 'No developers found'});
+    if (!developers) return res.status(404).json({ message: 'No developers found' });
 
     const highestCount = Math.max(...developers.map((developer) => developer.count));
 
     const currentDeveloper = developers.find((developer) => developer.current);
 
     const filteredDevelopers = developers.filter((developer) => developer.count < highestCount)
-    if(filteredDevelopers.length === 0) filteredDevelopers.push(...developers);
+    if (filteredDevelopers.length === 0) filteredDevelopers.push(...developers);
 
     const selectedDeveloper = filteredDevelopers[Math.floor(Math.random() * filteredDevelopers.length)];
     console.log("selected: ", selectedDeveloper?.name)
 
-const currentDate = new Date().toISOString();
+    const currentDate = new Date().toISOString();
     // Insert the log into the database
-  await sql`
+    await sql`
   INSERT INTO logs (backup_deployer, current_deployer, date, executed_by)
   VALUES (${currentDeveloper?.slack_id || "not applicable"}, ${selectedDeveloper.slack_id}, ${currentDate}, ${req.body.user_name})`;
 
@@ -102,18 +98,18 @@ const currentDate = new Date().toISOString();
 
 
 
-  return res.status(200).json(
-    {
-      response_type: 'in_channel',
-      text: `This Week's CDK Deployer:  <@${selectedDeveloper.slack_id}> | Backup: <@${currentDeveloper?.slack_id}>.`,
-    }
-  );
+    return res.status(200).json(
+      {
+        response_type: 'in_channel',
+        text: `This Week's CDK Deployer:  <@${selectedDeveloper.slack_id}> | Backup: <@${currentDeveloper?.slack_id}>.`,
+      }
+    );
 
     // TODO: add a check that returns who is the current developer
     // TODO: add some sort of log where it'd be ordered by date, to keep track of dates as the "current" might not be enough
-    
+
   } catch (error) {
-    return res.status(500).json({message: 'Internal server error', error: JSON.stringify(error)});
+    return res.status(500).json({ message: 'Internal server error', error: JSON.stringify(error) });
   }
 }
 
@@ -124,7 +120,7 @@ const revertHandler = async function (
   // TODO: refactor this and mix it
   const authorizedUsers = ["U0467DXP7D5", "U065JMM3WEB"]
   if (!authorizedUsers.includes(req.body.user_id))
-    return res.status(200).json({text: 'You are not authorized to run this command.'});
+    return res.status(200).json({ text: 'You are not authorized to run this command.' });
   try {
     const { rows: developers } = await sql`SELECT * FROM developers`;
     if (!developers) return res.status(404).json({ message: 'No developers found' });
@@ -169,7 +165,7 @@ const currentHandler = async function (
     // if (!developers) return res.status(404).json({ message: 'No developers found' });
     // const currentDeveloper = developers.find((developer) => developer.current);
 
-    if(!logs) return res.status(404).json({ message: 'No logs found' })
+    if (!logs) return res.status(404).json({ message: 'No logs found' })
     const lastLog = logs[0];
 
     // if(!currentDeveloper) return res.status(404).json({ message: 'No current developer found' })
@@ -183,5 +179,5 @@ const currentHandler = async function (
     );
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error', error: JSON.stringify(error) });
-  } 
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import Head from "next/head";
 import Hero from "./api/components/hero";
 import Pricing from "./api/components/pricing";
 import FeaturesGrid from "./api/components/features";
-import Navbar from "./api/components/navbar";
 
 export default function App() {
   return (
