c8ef6f3147cc63504ccdc035b5877eeb91c44ae3
Chiyen Venegas <chi@marandino.dev>
2024-08-23 12:23:27 -0600

    refactors: get rotation users api

--- src/app/api/v1/[organizationName]/[rotationName]/route.ts
+++ src/app/api/v1/[organizationName]/[rotationName]/route.ts
@@ -2,27 +2,23 @@ import { type NextRequest, NextResponse } from 'next/server';
 import { PostgresClient } from "utils/database";
 
 export async function GET(
-    _: NextRequest,
-    { params }: { params: { organizationName: string; rotationName: string; }; }
+  _: NextRequest,
+  { params }: { params: { organizationName: string; rotationName: string; }; }
 ) {
-    const { organizationName, rotationName } = params;
-    console.log(`Organization: ${organizationName}, Rotation: ${rotationName} requested all the user on the rotation`);
+  const { organizationName, rotationName } = params;
+
+  try {
     const DbClient = new PostgresClient(organizationName, rotationName);
+    const users = await DbClient.queryUsersForOrganizationAndRotation(organizationName, rotationName);
 
-    try {
-        const users = await DbClient.queryUsersForOrganizationAndRotation(organizationName, rotationName);
+    return NextResponse.json(users);
+  } catch (error) {
+    console.error('Error querying database:', error);
 
-        return NextResponse.json(
-            { users },
-            { status: 200 }
-        );
-    } catch (error) {
-        console.error('Error querying database:', error);
-
-        // Return an error response if something goes wrong
-        return NextResponse.json(
-            { error: 'Failed to query users' },
-            { status: 500 }
-        );
-    }
+    // Return an error response if something goes wrong
+    return NextResponse.json(
+      { error: 'Failed to query users' },
+      { status: 500 }
+    );
+  }
 }
