744509dc88f4e0070f77fda036c3bf9c2f8f7e4c
Chiyen Venegas <chi@marandino.dev>
2024-08-24 01:21:55 -0600

    adds: features copywriting and mobile support

--- src/pages/api/components/features.tsx
+++ src/pages/api/components/features.tsx
@@ -11,10 +11,11 @@ interface Feature {
 
 interface FeatureBoxProps {
   feature: Feature;
+  index: number;
 }
 
-const FeatureBox: React.FC<FeatureBoxProps> = ({ feature }) => (
-  <div className="p-6 rounded-lg shadow-md flex flex-col items-center text-center">
+const FeatureBox: React.FC<FeatureBoxProps> = ({ feature, index }) => (
+  <div className={`p-6 rounded-lg shadow-md flex flex-col items-center text-center  ${index > 3 && 'hidden md:flex'}`}>
     <div className="text-4xl mb-4">{feature.icon}</div>
     <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
     <p className="">{feature.description}</p>
@@ -23,23 +24,23 @@ const FeatureBox: React.FC<FeatureBoxProps> = ({ feature }) => (
 
 const FeaturesGrid: React.FC = () => {
   const features: Feature[] = [
-    { icon: <FaSlack />, title: 'All in Slack', description: 'Do everything in Slack, no need to leave the app.' },
-    { icon: <FaClock />, title: 'Scheduled rotations', description: 'Scheduled rotations for automatic assignements.' },
-    { icon: <FaSuitcase />, title: 'Workday rotations', description: 'Rotate on a per workday basis, for example, Monday to Friday.' },
-    { icon: <FaAddressCard />, title: 'Holidays and weekends', description: 'Full holidays and festivals support, to skip duty when you should rest!' },
-    { icon: <FaSlidersH />, title: 'Multi-duty rotations', description: 'Set multiple users on duty at the same time.' },
-    { icon: <GiIsland />, title: 'Vacation mode', description: 'Skip duty based on your status. For example, when you are on vacation.' },
-    { icon: <GiPadlock />, title: 'Privacy and control', description: 'Control access to rotations, and keep your data private.' },
-    { icon: <GiPlanetConquest />, title: 'Timezones', description: 'Full support for timezones to rotate always at the right time.' },
+    { icon: <FaSlack />, title: 'Slack-Native', description: 'Manage rotations without leaving Slack.' },
+    { icon: <FaSlidersH />, title: 'Multi-User Duty', description: 'Assign multiple users simultaneously for complex tasks.' },
+    { icon: <FaAddressCard />, title: 'Holiday-Aware', description: 'Skip assignments on holidays and weekends automatically.' },
+    { icon: <GiIsland />, title: 'Smart Availability', description: 'Auto-skip users based on vacation or status updates.' },
+    { icon: <GiPadlock />, title: 'Secure & Private', description: 'Full control over rotation access and data privacy.' },
+    { icon: <FaSuitcase />, title: 'Workday Focus', description: 'Customize rotations for business days (e.g., Monday-Friday).' },
+    { icon: <FaClock />, title: 'Automated Scheduling', description: 'Set it and forget it - assignments happen automatically.' },
+    { icon: <GiPlanetConquest />, title: 'Global-Friendly', description: 'Timezone support ensures timely rotations worldwide.' }
   ];
 
   return (
     <section>
-      <div className="px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-center mb-8">Awesome Features</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="px-4 pb-8 sm:px-6 lg:px-8">
+        <h2 className="text-2xl md:text-3xl font-bold text-center hidden md:block mb-8">Awesome Features</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-6">
           {features.map((feature, index) => (
-            <FeatureBox key={index} feature={feature} />
+            <FeatureBox key={index} index={index} feature={feature} />
           ))}
         </div>
       </div>
