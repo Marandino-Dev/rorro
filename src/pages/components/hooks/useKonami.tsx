6676c7569fe298395b0ef1150522fdb27da4299c
Chiyen Venegas <chi@marandino.dev>
2024-08-24 17:27:14 -0600

    adds: konami code behaviour

--- src/pages/components/hooks/useKonami.tsx
+++ src/pages/components/hooks/useKonami.tsx
@@ -1,12 +1,53 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+
+const u = 'ArrowUp';
+const d = 'ArrowDown';
+const l = 'ArrowLeft';
+const r = 'ArrowRight';
+const b = 'KeyB';
+const a = 'KeyA';
 
 export function useKonamiCode(): void {
-  const [isOnline, setIsOnline] = useState<boolean>(true);
 
-  console.log(isOnline);
+  const userInput: string[] = [];
+
+  const konamiCodeArray = [u, u, d, d, l, r, l, r, b, a];
+
+  // TODO: remove that any, somehow the Event type doesn't contain the same stuff as the KeyboardEvent but the KeyboardEvent
+  // is not available to be passed on the addeventlistener...
+  //eslint-disable-next-line
+  function handleKeyPress(this: HTMLElement, e: any) {
+
+    userInput.push(e.code);
+    if (userInput.length > konamiCodeArray.length) {
+      userInput.shift();
+
+    }
+
+
+
+    if (userInput.toString() == konamiCodeArray.toString()) {
+
+      new Audio('/success_sound.ogg').play();
+
+      const logo = document.querySelector('#rorroLogo') as HTMLImageElement;
+      const logoText = document.querySelector('#rorroText');
+
+      if (logo && logoText) {
+        logo.src = 'https://cdn-icons-png.flaticon.com/256/3819/3819238.png'; // Change this to the path of your new image
+        logoText.textContent = 'MONITO';
+      }
+      console.log(' You\'ve entered the MONITO code');
+    }
+  }
 
   useEffect(() => {
-    setIsOnline(false);
-    console.log(isOnline);
-  }, [isOnline]);
+    //on run
+    document.addEventListener('keydown', handleKeyPress);
+
+    // when unmounted
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 }
