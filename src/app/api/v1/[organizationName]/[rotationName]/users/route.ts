c9dcab920a2f71f406bdcde6daac0b8161b3ee89
K3iiX <karaya@marandino.dev>
2024-08-27 14:51:58 -0600

    feat: Add PUT handler to update user information in PostgreSQL

---
+++ src/app/api/v1/[organizationName]/[rotationName]/users/route.ts
@@ -0,0 +1,37 @@
+import { type NextRequest, NextResponse } from 'next/server';
+import { PostgresClient } from 'utils/database';
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { organizationName: string; rotationName: string } }
+) {
+  const { organizationName, rotationName } = params;
+  const updatedData = await request.json();
+
+  const client = new PostgresClient(organizationName, rotationName);
+
+  try {
+    const updatedUser = await client.updateUser(
+      updatedData.slack_id,
+      updatedData
+    );
+
+    if (!updatedUser) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: 'User updated', user: updatedUser });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { message: 'Error updating user', error: error.message },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: 'Unknown error occurred' },
+        { status: 500 }
+      );
+    }
+  }
+}
