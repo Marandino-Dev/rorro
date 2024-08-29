e48fffff899a6ea75d9e3282c5f790bf0bd0a90f
K3iiX <karaya@marandino.dev>
2024-08-28 18:28:31 -0600

    fix: merge issue

--- src/app/api/v1/[organizationName]/[rotationName]/users/route.ts
+++ src/app/api/v1/[organizationName]/[rotationName]/users/route.ts
@@ -1,6 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { PostgresClient } from 'utils/database';
 
+
 export async function PUT(
   request: NextRequest,
   { params }: { params: { organizationName: string; rotationName: string } }
@@ -33,5 +34,27 @@ export async function PUT(
         { status: 500 }
       );
     }
+  }
+}
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: { organizationName: string; rotationName: string; }; }
+) {
+  const { organizationName, rotationName } = params;
+
+  try {
+    const DbClient = new PostgresClient(organizationName, rotationName);
+    const users = await DbClient.queryUsersForOrganizationAndRotation(organizationName, rotationName);
+
+    return NextResponse.json(users);
+  } catch (error) {
+    console.error('Error querying database:', error);
+
+    // Return an error response if something goes wrong
+    return NextResponse.json(
+      { error: 'Failed to query users' },
+      { status: 500 }
+    );
   }
 }
