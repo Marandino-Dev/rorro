cbe757dd0b7c7f109c03f7608dfecf7df903953a
Deyner Cruz <nenedac98@gmail.com>
2024-08-31 09:00:04 -0600

    adds: logs when changes are done in dashboard
    - to do: specify the data that was modified

--- src/app/api/v1/[organizationName]/[rotationName]/users/route.ts
+++ src/app/api/v1/[organizationName]/[rotationName]/users/route.ts
@@ -1,40 +1,6 @@
+import { createLog } from 'utils/logic';
 import { type NextRequest, NextResponse } from 'next/server';
 import { PostgresClient } from 'utils/database';
-
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: { organizationName: string; rotationName: string } }
-) {
-  const { organizationName, rotationName } = params;
-  const { updateData, userId } = await request.json();
-
-  const client = new PostgresClient(organizationName, rotationName);
-
-  try {
-    const updatedUser = await client.updateUser(
-      userId,
-      updateData
-    );
-
-    if (!updatedUser) {
-      return NextResponse.json({ message: 'User not found' }, { status: 404 });
-    }
-
-    return NextResponse.json({ message: 'User updated', user: updatedUser });
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json(
-        { message: 'Error updating user', error: error.message },
-        { status: 500 }
-      );
-    } else {
-      return NextResponse.json(
-        { message: 'Unknown error occurred' },
-        { status: 500 }
-      );
-    }
-  }
-}
 
 export async function GET(
   _: NextRequest,
@@ -57,3 +23,50 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { organizationName: string; rotationName: string } }
+) {
+  const { organizationName, rotationName } = params;
+  const { updateData, userId } = await request.json();
+
+  const client = new PostgresClient(organizationName, rotationName);
+
+  try {
+    const updatedUser = await client.updateUser(
+      userId,
+      updateData
+    );
+
+    if (!updatedUser) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    // Create a log entry
+    const logEntry = createLog(
+      `Updated user properties of: ${updatedUser.full_name}'}.`,
+      'Dashboard',
+      'status'
+    );
+
+    // Insert the log entry into the database
+    await client.insertLog(organizationName, rotationName, logEntry);
+
+    return NextResponse.json({ message: 'User updated', user: updatedUser });
+
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { message: 'Error updating user', error: error.message },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: 'Unknown error occurred' },
+        { status: 500 }
+      );
+    }
+  }
+}
+
