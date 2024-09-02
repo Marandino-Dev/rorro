9db5f8f632c641e8c8c4184421bdbc5110f1be8c
Deyner Cruz <158535310+nenedac@users.noreply.github.com>
2024-09-02 11:10:33 -0600

    RR-53 Log Update For User Data (#32)
    * adds: logs when changes are done in dashboard
    
    * feature: dashboard log display specific changes
    
    * fixes: unintended filtering of on_holiday users
    
    ---------
    
    Co-authored-by: Chiyen Venegas <chi@marandino.dev>

--- src/app/api/v1/[organizationName]/[rotationName]/users/route.ts
+++ src/app/api/v1/[organizationName]/[rotationName]/users/route.ts
@@ -1,40 +1,7 @@
+import { createLog } from 'utils/logic';
 import { type NextRequest, NextResponse } from 'next/server';
 import { PostgresClient } from 'utils/database';
-
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: { organizationName: string; rotationName: string } }
-) {
-  const { organizationName, rotationName } = params;
-  const { updateData, userId } = await request.json();
-
-  const client = new PostgresClient(organizationName, rotationName);
-
-  try {
-    const updatedUser = await client.updateUser(
-      userId,
-      updateData
-    );
-
-    if (!updatedUser) {
-      return NextResponse.json({ message: 'User not found' }, { status: 404 });
-    }
-
-    return NextResponse.json({ message: 'User updated', user: updatedUser });
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json(
-        { message: 'Error updating user', error: error.message },
-        { status: 500 }
-      );
-    } else {
-      return NextResponse.json(
-        { message: 'Unknown error occurred' },
-        { status: 500 }
-      );
-    }
-  }
-}
+import { SlackUser } from 'types';
 
 export async function GET(
   _: NextRequest,
@@ -57,3 +24,57 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { organizationName: string; rotationName: string } }
+) {
+  const { organizationName, rotationName } = params;
+  const { updateData, userId } = await request.json();
+  const client = new PostgresClient(organizationName, rotationName);
+  const { rows } = await client.queryUsersForOrganizationAndRotation(organizationName, rotationName);
+  try {
+    const updatedUser = await client.updateUser(
+      userId,
+      updateData
+    );
+    const originalUser = rows.find(user => user.slack_id === updatedUser?.slack_id);
+
+    const changes: string[] = []; // Define `changes` in an outer scope
+
+    if (originalUser && updatedUser) {
+      Object.entries(originalUser).forEach(([key, originalValue]) => {
+        const updatedValue = updatedUser[key as keyof SlackUser];
+        if (originalValue !== updatedValue) {
+          changes.push(`${key}: ${updatedValue}`);
+        }
+      });
+    }
+
+    // Create a log entry
+    const logEntry = createLog(
+      `Changes made to ${updatedUser?.full_name}: ${changes.join(', ')}.`,
+      'Dashboard',
+      'status'
+    );
+
+    // Insert the log entry into the database
+    await client.insertLog(organizationName, rotationName, logEntry);
+
+    return NextResponse.json({ message: 'User updated', user: updatedUser });
+
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { message: 'Error updating user', error: error.message },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: 'Unknown error occurred' },
+        { status: 500 }
+      );
+    }
+  }
+}
+
