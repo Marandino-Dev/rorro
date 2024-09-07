3ba39617ff9f72614536803c7d8445fd743ac24f
K3iiX <karaya@marandino.dev>
2024-09-06 20:50:41 -0600

    fix: single quotes lint error

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Modal from './api/components/modal';
-import ContentLoader from "react-content-loader";
+import ContentLoader from 'react-content-loader';
 
 import {
   Table,
