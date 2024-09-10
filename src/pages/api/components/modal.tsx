0674c404a6147d4eda6f48f168bb828178059677
Kedwin Araya Gomez <159331750+K3iiX@users.noreply.github.com>
2024-09-10 03:47:21 -0600

    RR-62 - Dashboard Tables Enhancement (#43)
    * feat: send updated fields instead the entire SlackUser
    
    * adds: toggles instead checkBoxes
    
    * fixes: missing dependencies and indent issues
    
    * adds: sorting functionality
    
    * setup: count field as a ref when the modal
    
    * fix: lint error with the import order
    
    * adds: row outline for users with on_holiday property
    
    * fixes: modal style and table colors
    
    * adds: pagination functionality
    
    * fixes: dashboard colors and modal details
    
    * adds: base skeleton placeholder to the dashboard
    
    * fix: single quotes lint error
    
    * adds: loader component
    
    * adds: jsx default formatter for vscode config
    
    * adds: loader component to dashboard and moves the table structure away from map
    
    * refact: adjust the vertical padding on table cells
    
    * fixes; content snapping into place and date formatting
    
    * chore: regenerates package-lock
    
    * fixes: dependencies issues
    
    * refactor pagination and sorting
    
    * hotfix: linting
    
    * removes: unnecessary timeout
    
    * fixes: snapping before loading the dashboard
    
    * fixes: quadruplicated data and long logs descriptions
    
    * fixes: inconsistent data display
    
    ---------
    
    Co-authored-by: Chiyen Venegas <chi@marandino.dev>

--- src/pages/api/components/modal.tsx
+++ src/pages/api/components/modal.tsx
@@ -23,11 +23,22 @@ const Modal: React.FC<ModalProps> = ({
     }
   }, [user]);
 
+  // CHANGED FIELDS
+
+  const [changedFields, setChangedFields] = useState<Partial<SlackUser>>({});
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value, type, checked } = e.target;
+    const newValue = type === 'checkbox' ? checked : value;
+
     setFormData((prevData) => ({
       ...(prevData as SlackUser),
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: newValue,
+    }));
+
+    setChangedFields((prevChanges) => ({
+      ...prevChanges,
+      [name]: newValue,
     }));
   };
 
@@ -35,7 +46,7 @@ const Modal: React.FC<ModalProps> = ({
     e.preventDefault();
     if (!formData) return;
 
-    const BASE_API_URL = window.location.origin + '/api/v1' ||'http://localhost:3000/api/v1';
+    const BASE_API_URL = window.location.origin + '/api/v1' || 'http://localhost:3000/api/v1';
 
     setIsSubmitting(true);
     try {
@@ -47,7 +58,7 @@ const Modal: React.FC<ModalProps> = ({
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            updateData: formData,
+            updateData: changedFields,
             userId: formData.slack_id,
           }),
         }
@@ -70,11 +81,36 @@ const Modal: React.FC<ModalProps> = ({
     }
   };
 
+  // TOGGLES
+  const renderToggle = (id: string, name: keyof SlackUser, label: string) => {
+    const isChecked = !!formData?.[name];
+
+    return (
+      <div className="flex items-center space-x-4">
+        <label htmlFor={id} className="inline-flex items-center cursor-pointer">
+          <span className="mr-3 text">{label}</span>
+          <div className="relative">
+            <input
+              type="checkbox"
+              id={id}
+              name={name}
+              checked={isChecked}
+              onChange={handleChange}
+              className="sr-only peer"
+            />
+            <div className="w-11 h-6 bg-red-500 rounded-full peer-checked:bg-secondary"/>
+            <div className="absolute top-0.5 left-0.5 bg-light-bg w-5 h-5 rounded-full transition-transform transform peer-checked:translate-x-full"/>
+          </div>
+        </label>
+      </div>
+    );
+  };
+
   // MODAL HERO
 
   return (
     <div className='fixed inset-x-0 top-0 h-full flex items-start justify-center'>
-      <div className='absolute inset-0 z-10 bg-gray-800 bg-opacity-50' onClick={onClose}></div>
+      <div className='absolute inset-0 z-10 bg-gray-800 bg-opacity-50' onClick={onClose}/>
       <div className='relative z-30 bg-light-bg p-6 sm:p-8 rounded-lg shadow-lg max-w-sm w-full mt-12 md:max-w-lg md:my-auto'>
         {/* CLOSE BUTTON */}
         <button
@@ -85,77 +121,50 @@ const Modal: React.FC<ModalProps> = ({
         </button>
         <h2 className='text text-2xl font-semibold mb-6'>Update User</h2>
         <form onSubmit={handleSubmit}>
-          <div className='mb-4'>
-            <label
-              className='block text text-sm font-bold mb-2'
-              htmlFor='full_name'
-            >
-              Full Name
-            </label>
-            <input
-              type='text'
-              id='full_name'
-              name='full_name'
-              value={formData?.full_name || ''}
-              onChange={handleChange}
-              className='w-full px-3 py-2 border text rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black'
-            />
+
+          {/* NAME & COUNT IN THE SAME ROW */}
+          <div className='flex space-x-4 mb-4'>
+            <div className='w-7/12'>
+              <label
+                className='block text text-sm font-bold mb-2'
+                htmlFor='full_name'
+              >
+                Full Name
+              </label>
+              <input
+                type='text'
+                id='full_name'
+                name='full_name'
+                value={formData?.full_name || ''}
+                onChange={handleChange}
+                className='w-full px-3 py-2 border text rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black'
+              />
+            </div>
+            <div className='w-5/12'>
+              <label
+                className='block text text-sm font-bold mb-2'
+                htmlFor='count'
+              >
+                Count
+              </label>
+              <input
+                type='number'
+                id='count'
+                name='count'
+                value={formData?.count || ''}
+                onChange={handleChange}
+                className='w-full px-3 py-2 border text rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black'
+              />
+            </div>
           </div>
-          <div className='mb-4'>
-            <label
-              className='block text text-sm font-bold mb-2'
-              htmlFor='count'
-            >
-              Count
-            </label>
-            <input
-              type='number'
-              id='count'
-              name='count'
-              value={formData?.count || ''}
-              onChange={handleChange}
-              className='w-full px-3 py-2 border text rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black'
-            />
+
+          {/* TOGGLES */}
+          <div className="flex space-x-4 mb-4">
+            {renderToggle('on_holiday', 'on_holiday', 'On Holiday')}
+            {renderToggle('on_duty', 'on_duty', 'On Duty')}
+            {renderToggle('on_backup', 'on_backup', 'On Backup')}
           </div>
-          <div className='mb-4 flex items-center'>
-            <input
-              type='checkbox'
-              id='on_holiday'
-              name='on_holiday'
-              checked={formData?.on_holiday || false}
-              onChange={handleChange}
-              className='mr-2'
-            />
-            <label htmlFor='on_holiday' className='text'>
-              On Holiday
-            </label>
-          </div>
-          <div className='mb-4 flex items-center'>
-            <input
-              type='checkbox'
-              id='on_duty'
-              name='on_duty'
-              checked={formData?.on_duty || false}
-              onChange={handleChange}
-              className='mr-2'
-            />
-            <label htmlFor='on_duty' className='text'>
-              On Duty
-            </label>
-          </div>
-          <div className='mb-4 flex items-center'>
-            <input
-              type='checkbox'
-              id='on_backup'
-              name='on_backup'
-              checked={formData?.on_backup || false}
-              onChange={handleChange}
-              className='mr-2'
-            />
-            <label htmlFor='on_backup' className='text'>
-              On Backup
-            </label>
-          </div>
+
           <div className='flex justify-end'>
             <button
               type='submit'
