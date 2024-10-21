aeb5682973d67e845c9b7cdd65b42315c2ac28da
Chiyen Venegas <chi@marandino.dev>
2024-10-21 06:34:18 -0600

    adds: pick command

---
+++ src/app/api/v1/slack/pick/route.ts
@@ -0,0 +1,115 @@
+import { PostgresClient, TableName } from 'utils/database';
+import { SlackUser } from 'types';
+import { NextRequest, NextResponse } from 'next/server';
+import {
+  SlackResponseType,
+  getSlackMessage,
+  getSlackUsersFromChannel,
+  getSlackUsersFromUserGroup,
+  parsePayloadFromRequest,
+  sanitizeSlackText,
+} from 'utils/slack';
+import { createLog, filterSlackUsers, filterUserOnDuty, selectSlackUser } from '@/app/api/_utils/logic';
+
+// TODO: refactor all this...
+export async function POST(req: NextRequest) {
+  try {
+    const parsedPayload = await parsePayloadFromRequest(req);
+
+    // Destructure necessary values from the payload
+    const { text, team_domain, channel_id, team_id, user_name, user_id } = parsedPayload;
+
+    // Clean up the command text
+    const command: string = text.replace(/<[^>]+>/g, '');
+
+    // Extract the userGroup ID if present
+    const userGroupMatch = text.match(/<!subteam\^([A-Z0-9]+)\|/);
+    const userGroup = userGroupMatch ? userGroupMatch[1] : null;
+
+    const inputUrlMatch = text.match(/<https:.*?>/);
+    const inputUrl = inputUrlMatch ? inputUrlMatch[0] : null;
+
+    if (inputUrl) {
+      console.log('This command has an url', inputUrl);
+    }
+
+    // Sanitize and prepare values for database operations
+    const organizationName = sanitizeSlackText(team_domain);
+    const rotationName = sanitizeSlackText(command);
+    const channelId = channel_id.trim();
+
+    // for any of the errors use the trigger_id to create a modal and request them to create a pick selection
+
+    // Validate rotation name
+    if (!rotationName || typeof rotationName !== 'string') {
+      console.error('Invalid rotationName:', rotationName);
+      return NextResponse.json(
+        getSlackMessage(SlackResponseType.Ephemeral, 'a name for the task to pick is required')
+      );
+    }
+
+    const DbClient = new PostgresClient(organizationName, rotationName);
+    const taskExists = await DbClient.confirmTaskExists();
+
+    if (!taskExists) {
+      console.log('I should be creating a task');
+
+      // Get the users from the Slack channel or the userGroup if it was passed
+      const newUsers = userGroup ?
+        await getSlackUsersFromUserGroup(userGroup, team_id):
+        await getSlackUsersFromChannel(channelId, team_id);
+
+      if (!newUsers || newUsers.length === 0) {
+        console.error('No users found:', newUsers);
+        return NextResponse.json(
+          getSlackMessage(SlackResponseType.Ephemeral, 'Could not retrieve any of the users, if this is a private channel add an @userGroup or invite Rorro to the channel.')
+        );
+      }
+      await DbClient.putItems<SlackUser>(newUsers, TableName.Users);
+
+      await DbClient.createLogsTableIfNotExists();
+
+      const logEntry = createLog(
+        `Created ${rotationName} task`,
+        sanitizeSlackText(user_name || user_id),
+        'status'
+      );
+      await DbClient.insertLog(logEntry);
+    }
+
+    const { rows } = await DbClient.queryUsersForOrganizationAndRotation(organizationName, rotationName, true);
+
+    const previousBackup = rows.find(user => user.on_backup === true)?.slack_id;
+    const newBackup = rows.find(user => user.on_duty === true);
+
+    const usersNotOnDuty = filterUserOnDuty(rows);
+    const filteredUsers = filterSlackUsers(usersNotOnDuty);
+
+    const userOnDuty: SlackUser = selectSlackUser(filteredUsers);
+
+    // TODO: refactor everything here...
+    await DbClient.rotateUsers(
+      userOnDuty.slack_id,
+      newBackup?.slack_id,
+      previousBackup
+    );
+
+    const logEntry = createLog(
+      `Picked ${userOnDuty.full_name} for ${rotationName}`,
+      sanitizeSlackText(user_name),
+      'rotation'
+    );
+
+    await DbClient.insertLog(logEntry);
+
+    const slackMessage = getSlackMessage(
+      SlackResponseType.InChannel,
+      `<@${userOnDuty.slack_id}> has been selected for ${rotationName.replace(/_/g, ' ')}${inputUrl ? `\n${inputUrl}` : ''}`
+    );
+    return NextResponse.json(slackMessage);
+  } catch (error) {
+    return NextResponse.json(
+      getSlackMessage(SlackResponseType.Ephemeral, 'error: Processing request' + JSON.stringify(error, null, 2))
+    );
+  }
+}
